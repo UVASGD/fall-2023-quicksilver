413fa9ebd86a774182b4dda66ca37b5b757fed9c
Thomas Arnold <ybn4aq@virginia.edu>
2023-09-17 22:51:50 -0400

    For some reason the buttons can't be clicked. I have no idea why

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -10,10 +10,6 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("LevelSelect");
     }
-    public void LoadLevel1()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
 
     public void Quit()
     {
