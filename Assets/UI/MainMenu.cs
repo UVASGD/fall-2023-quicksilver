250c34c7aab9795896479216f977fc20e12a0b8a
Thomas Arnold <ybn4aq@virginia.edu>
2023-09-21 21:07:40 -0400

    Functional level select submenu

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -5,14 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject StartButton;
+    [SerializeField]
+    private GameObject BackButton;
+    [SerializeField]
+    private GameObject[] LevelButtons;
+    [SerializeField]
+    private GameObject QuitButton;
 
-    public void LoadLevelSelect()
+    private void Start()
     {
-        SceneManager.LoadScene("LevelSelect");
+        Back();
+    }
+
+    public void StartGame()
+    {
+        StartButton.SetActive(false);
+        BackButton.SetActive(true);
+        for (int i = 0; i < LevelButtons.Length; i++)
+        {
+            LevelButtons[i].SetActive(true);
+        }
+    }
+
+    public void LevelSelect() // TODO: maybe in the future make this take an int level or maybe enum level as an arg to specifically load a certain level?
+    {
+        SceneManager.LoadScene("Level1");
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    // Hide level select and back, show Start and Quit
+    public void Back()
+    {
+        for (int i = 0; i < LevelButtons.Length; i++)
+        {
+            LevelButtons[i].SetActive(false);
+        }
+        BackButton.SetActive(false);
+        StartButton.SetActive(true);
+        QuitButton.SetActive(true);
     }
 }
